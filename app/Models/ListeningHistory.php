7a8025fcff00ec759b74749327eda34fa1897328
Ghina Shour <shourghina@gmail.com>
2025-08-10 10:40:20 +0300

    adding models , migrations , factories and seeders

---
+++ app/Models/ListeningHistory.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ListeningHistory extends Model
+{
+    /** @use HasFactory<\Database\Factories\ListeningHistoryFactory> */
+    use HasFactory;
+}
